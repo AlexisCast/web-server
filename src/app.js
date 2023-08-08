929ada065f70e4f2e33f055b5a1189b4714ea917
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-07 18:55:35 -0700

    web servers

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+const port = 3000;
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+	res.send("<h1>Weather</h1>");
+});
+
+app.get("/help", (req, res) => {
+	res.send({
+		hello: "test",
+	});
+});
+
+app.get("/about", (req, res) => {
+	res.send("<h1>About</h1>");
+});
+
+app.get("/weather", (req, res) => {
+	res.send({
+		forecast: "forecast",
+		location: "Philadelphia",
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Server is up on ${port}`);
+});
