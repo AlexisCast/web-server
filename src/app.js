a1367ea7db88793f64f9e18b2cc07397ef9272e5
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-09 12:11:56 -0700

    add query string

--- src/app.js
+++ src/app.js
@@ -41,9 +41,26 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+	if (!req.query.address) {
+		return res.send({
+			error: "You must provide address",
+		});
+	}
 	res.send({
 		forecast: "forecast",
 		location: "Philadelphia",
+		address: req.query.address,
+	});
+});
+
+app.get("/products", (req, res) => {
+	if (!req.query.search) {
+		return res.send({
+			error: "You must provide a search term",
+		});
+	}
+	res.send({
+		products: [],
 	});
 });
 
