d91506b4f3d6ffb9ee507b5dfe4d9ea240495615
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-09 17:55:53 -0700

    client side http request with fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-console.log('Client side javascript file is loaded!')
+//client side JS
+
+console.log("Client side javascript file is loaded!");
+
+fetch("http://localhost:3000/weather?address=!").then((response) => {
+	response.json().then((data) => {
+		if (data.error) {
+			console.log(data.error);
+		} else {
+			console.log(data.location);
+			console.log(data.forecast);
+			// console.log(data.address);
+		}
+	});
+});
